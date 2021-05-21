5bab4e866bdefedcd61871788f78f7267224336c
viram87 <viramchoksi77166@gmail.com>
2021-05-21 14:42:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagepicker/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.imagepicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.BitSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int CAMERA_PERMISSION_CODE = 101;
+    public static final int CAMERA_REQ_CODE = 102;
+    int SELECT_PHOTO = 1;
+    Uri uri;
+    Button btnSelect;
+    ImageView imgView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imgShow);
+        btnSelect = findViewById(R.id.btn);
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, SELECT_PHOTO);
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                imgView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
+}
+
